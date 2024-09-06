b2506e86ddd61927979de49b0000774860380e11
Michael Bøcker-Larsen <mbl@code.boutique>
2024-09-06 13:02:25 +0800

    style: format

--- .config/nvim/lua/custom/plugins/fzf-lua.lua
+++ .config/nvim/lua/custom/plugins/fzf-lua.lua
@@ -1,11 +1,11 @@
 --- https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#commands
 --- :FzfLua builtins
 return {
-  'ibhagwan/fzf-lua',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require('fzf-lua').setup {
-      'borderless',
+    require("fzf-lua").setup {
+      "borderless",
     }
   end,
 }
