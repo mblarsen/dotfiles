7243bd2313f16af3716190d9d535b069f2ddf484
Michael Bøcker-Larsen <mbl@code.boutique>
2024-09-12 09:06:32 +0545

    feat: silence some noice output

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -10,18 +10,23 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
+        hover = {
+          silent = true,
+        },
+        progress = {
+          enabled = false,
+        },
       },
       messages = {
         view = "mini",
         view_warn = "mini",
       },
-      -- You can enable a preset for easier configuration
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = true, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
       },
     }
   end,
