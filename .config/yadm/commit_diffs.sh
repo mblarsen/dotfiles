64a6071b45d171e946ac89f8370eeef3d2238b51
Michael Bøcker-Larsen <mbl@code.boutique>
2024-11-22 18:32:46 +0800

    yadm: add dry-run option to commit script
    add a dry-run option to check the commit messages without making actual changes. this involves checking for `--dry-run` argument and modifying the commit and push process based on this flag.

--- .config/yadm/commit_diffs.sh
+++ .config/yadm/commit_diffs.sh
@@ -11,17 +11,35 @@ subjects=(
   zsh
 )
 
+dry_run=false
+
+for arg in "$@"; do
+  if [ "$arg" == "--dry-run" ]; then
+    dry_run=true
+    break
+  fi
+done
+
 for subject in "${subjects[@]}"; do
   commit_message=$(./commit_diff.sh "$subject")
   if [ -z "$commit_message" ]; then
     echo "No changes for $subject"
   else
-    echo "Committing changes for $subject"
-    yadm add ~/.config/$subject
-    yadm commit -m "$commit_message"
+    if [ "$dry_run" == true ]; then
+      echo "Committing changes for $subject (dry-run)"
+      echo "$commit_message"
+    else
+      echo "Committing changes for $subject"
+      echo "$commit_message"
+      yadm add ~/.config/$subject
+      yadm commit -m "$commit_message"
+    fi
   fi
 done
 
-echo "Pushing commits"
-
-yadm push
+if [ "$dry_run" == true ]; then
+  echo "Pushing commits (dry-run)"
+else
+  echo "Pushing commits"
+  yadm push
+fi
