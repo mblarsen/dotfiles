4332e75b13aa8985e40166e8c3ae25d434d4615b
Michael Bøcker-Larsen <mbl@code.boutique>
2024-11-22 13:27:27 +0800

    yadm: update commit_diff.sh script rules for commit message generation
    ---
    Commit message by OpenAI (gpt-4-turbo)

--- .config/yadm/commit_diff.sh
+++ .config/yadm/commit_diff.sh
@@ -11,11 +11,14 @@ Help me generate a succinct git commit message for this change.
 A few rules:
 - Only output the message, not meta chatter
 - Exclude permission changes
-- Use commitizen messages such as: feat(nvim) and refactor(wezterm). That is action and subject
-- If I do not provide a subject use the parent folder as the subject
-- Try to get the change into the subject of the commit message but not required, you can use the body as well if there are more changes than fits in the subject
+- For the commit messages I'd like the subject line to start with the subject
+  word I provide and if not provided use the parent folder as the subject. E.g.
+  "nvim: change colorscheme" or "wezterm: reduce font size"
+- Try to get the change into the subject line of the commit message but not required, 
+  you can use the body as well if there are more changes than fits in the subject
 - First I'll provide the 'Git Status:' and after 'Git Diff:' section for you to base the message upon
 - Finally add the message: Commit message by OpenAI ($model)
+  after three hyphens and a linebreak
 EOF
 
 git_status=$(yadm status ~/.config/$subject)
