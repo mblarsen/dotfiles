298485df6952a86a3c1f4d393a63b71d9a5c0fbb
Michael Bøcker-Larsen <mbl@code.boutique>
2024-11-22 17:06:22 +0800

    zsh: enhance plugin configurations and descriptions
    - added comments explaining each plugin in init.zsh
    - enabled 'zimfw/environment' with startup options
    - added prefixes for git aliases under 'zimfw/git'
    - provided utility functions via 'zimfw/utility'
    - expanded oh_my_zsh plugins with descriptions for 'direnv', 'fzf', 'vi-mode', 'gh', 'brew', and 'nvm'
    - removed 'git-commit' plugin from the list

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -7,35 +7,40 @@ zstyle ':zim:*' use-compinit false
 
 source "$ZPLUG_HOME/init.zsh"
 
-# https://github.com/zimfw/completion
-# conflict with compinit so disabled for now
-# may not need it
-# zplug "zimfw/completion", use:"init.zsh"
-
 # https://github.com/zimfw/environment
+# Sets sane Zsh built-in environment options.
 zplug "zimfw/environment", use:"init.zsh"
 
 # https://github.com/zimfw/git
+# Provides handy Git aliases and functions.
 zstyle ':zim:git' aliases-prefix 'g'
 zplug "zimfw/git", use:"init.zsh"  aliases-prefix 'g'
 
 # https://github.com/zimfw/utility
+# Utility aliases and functions.
 zplug "zimfw/utility", use:"init.zsh"
 
 from_oh_my_zsh=(
   # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/direnv
+  # This plugin creates the Direnv hook.
   direnv
   # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/fzf
+  # This plugin tries to find junegunn's fzf based on where it's been
+  # installed, and enables its fuzzy auto-completion and key bindings.
   fzf
   # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/vi-mode
+  # This plugin increase vi-like zsh functionality.
   vi-mode
   # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/gh
+  # This plugin adds completion for the GitHub CLI.
   gh
   # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/brew
+  # The plugin adds several aliases for common brew commands.
   brew
-  # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git-commit
-  git-commit
   # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/nvm
+  # This plugin adds autocompletions for nvm — a Node.js version manager. It
+  # also automatically sources nvm, so you don't need to do it manually in your
+  # .zshrc.
   nvm
 )
 
