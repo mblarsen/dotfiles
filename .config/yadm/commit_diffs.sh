a09fa84a6c34c15105a03b1dfef2ef695a11a3ed
Michael Bøcker-Larsen <mbl@code.boutique>
2024-11-22 15:31:26 +0800

    yadm: streamline commit_diffs.sh, optimize script structure
    - removed unnecessary blank lines and debug statements
    - replaced individual commit blocks with a loop to handle all subjects

--- .config/yadm/commit_diffs.sh
+++ .config/yadm/commit_diffs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 cd "$(dirname "$0")"
 
 subjects=(aerospace nvim wezterm yadm zsh)
@@ -10,36 +9,12 @@ for subject in "${subjects[@]}"; do
   if [ -z "$commit_message" ]; then
     echo "No changes for $subject"
   else
-    set -x
     echo "Committing changes for $subject"
     yadm add ~/.config/$subject
     yadm commit -m "$commit_message"
-    set +x
   fi
 done
 
 echo "Pushing commits"
 
 yadm push
-
-# cd "$(dirname "$0")"
-# 
-# commit_message=$(./commit_diff.sh aerospace)
-# yadm add ~/.config/aerospace
-# yadm commit -m "$commit_message"
-# 
-# commit_message=$(./commit_diff.sh nvim)
-# yadm add ~/.config/nvim
-# yadm commit -m "$commit_message"
-# 
-# commit_message=$(./commit_diff.sh wezterm)
-# yadm add ~/.config/wezterm
-# yadm commit -m "$commit_message"
-# 
-# commit_message=$(./commit_diff.sh yadm)
-# yadm add ~/.config/yadm
-# yadm commit -m "$commit_message"
-# 
-# commit_message=$(./commit_diff.sh zsh)
-# yadm add ~/.config/zsh
-# yadm commit -m "$commit_message"
