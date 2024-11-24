4485931b49aedfa78e36687dd1c61ea9fca5de88
Michael Bøcker-Larsen <mbl@code.boutique>
2024-11-25 00:00:21 +0800

    nvim: add repeat pounce command with 'f?' key binding
    added a new key binding for pounce with repeat functionality in pounce.lua. commented out several other potential bindings for further evaluation.

--- .config/nvim/lua/custom/plugins/pounce.lua
+++ .config/nvim/lua/custom/plugins/pounce.lua
@@ -7,5 +7,21 @@ return {
     map("n", "f/", function()
       require("pounce").pounce {}
     end)
+    map("n", "f?", function()
+      require("pounce").pounce { do_repeat = true }
+    end)
+    -- map("n", "S", function()
+    --   require("pounce").pounce { do_repeat = true }
+    -- end)
+    -- map("x", "s", function()
+    --   require("pounce").pounce {}
+    -- end)
+    -- map("o", "gs", function()
+    --   require("pounce").pounce {}
+    -- end)
+    -- map("n", "S", function()
+    --   require("pounce").pounce { input = { reg = "/" } }
+    -- end)
+    -- ?
   end,
 }
