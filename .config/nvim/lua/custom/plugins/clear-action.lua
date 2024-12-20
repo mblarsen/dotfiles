8989727f140068e2c6366d52622170e4da5175d8
Michael Bøcker-Larsen <mbl@code.boutique>
2024-12-20 21:58:46 +0800

    nvim: update clear-action plugin configuration
    - disable signs and labels in `clear-action` setup
    - refactor key mappings for code actions
    - implement filtering of unwanted actions
    - remove unused configurations and comments

--- .config/nvim/lua/custom/plugins/clear-action.lua
+++ .config/nvim/lua/custom/plugins/clear-action.lua
@@ -1,27 +1,45 @@
+local unwanted_actions = {
+  "Move to a new file",
+}
+
+-- Filter out unwanted actions
+local filter = function(action)
+  for _, unwanted in ipairs(unwanted_actions) do
+    if vim.startswith(action.title, unwanted) then
+      return false
+    end
+  end
+  return true
+end
+
 return {
   "luckasRanarison/clear-action.nvim",
   event = { "BufReadPre" },
   config = function()
     require("clear-action").setup {
       signs = {
-        enable = true,
+        enable = false,
         show_count = false,
-        -- show first action for current line
-        show_label = true,
-        -- icons = {
-        --   quickfix = " ",
-        --   refactor = " ",
-        --   source = " ",
-        --   combined = " ", -- used when combine is set to true or as a fallback when there is no action kind
-        -- },
+        show_label = false,
       },
       mappings = {
-        code_action = "<leader>cc",
-        apply_first = "<leader>ca",
-        -- refactor = '<leader>cr',
         source = "<leader>cs",
-        quickfix = "<leader>cq",
       },
     }
+
+    -- Apply first
+    vim.keymap.set("n", "<leader>ca", function ()
+      require("clear-action").code_action({
+        first = true,
+        filter = filter,
+      })
+    end, { noremap = true, silent = true })
+
+    -- List code actions
+    vim.keymap.set("n", "<leader>cc", function ()
+      require("clear-action").code_action({
+        filter = filter,
+      })
+    end, { noremap = true, silent = true })
   end,
 }
