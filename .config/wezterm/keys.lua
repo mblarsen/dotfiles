161cf8b3bf70c138de50bc1e1079fa3a25161c5b
Michael Bøcker-Larsen <mbl@code.boutique>
2025-02-07 00:53:27 +0800

    wezterm: disable keybindings
    Switching to TMUX again, hmm?

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -4,39 +4,39 @@ local helpers = require 'helpers'
 local M = {}
 
 function M.configure(config)
-  config.debug_key_events = true
+  -- config.debug_key_events = true
 
-  config.leader = {
-    key = 'b',
-    mods = 'CTRL',
-    timeout_milliseconds = 1000,
-  }
-
-  helpers.merge_keys(config, {
-    {
-      key = '%',
-      mods = 'LEADER',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = '"',
-      mods = 'LEADER',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'z',
-      mods = 'LEADER',
-      action = wezterm.action.TogglePaneZoomState,
-    },
-    {
-      key = ',',
-      mods = 'CMD',
-      action = wezterm.action.SpawnCommandInNewWindow {
-        cwd = os.getenv 'WEZTERM_CONFIG_DIR',
-        args = { os.getenv 'SHELL', '-c', '$EDITOR $WEZTERM_CONFIG_FILE' },
-      },
-    },
-  })
+  -- config.leader = {
+  --   key = 'b',
+  --   mods = 'CTRL',
+  --   timeout_milliseconds = 1000,
+  -- }
+  --
+  -- helpers.merge_keys(config, {
+  --   {
+  --     key = '%',
+  --     mods = 'LEADER',
+  --     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  --   },
+  --   {
+  --     key = '"',
+  --     mods = 'LEADER',
+  --     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  --   },
+  --   {
+  --     key = 'z',
+  --     mods = 'LEADER',
+  --     action = wezterm.action.TogglePaneZoomState,
+  --   },
+  --   {
+  --     key = ',',
+  --     mods = 'CMD',
+  --     action = wezterm.action.SpawnCommandInNewWindow {
+  --       cwd = os.getenv 'WEZTERM_CONFIG_DIR',
+  --       args = { os.getenv 'SHELL', '-c', '$EDITOR $WEZTERM_CONFIG_FILE' },
+  --     },
+  --   },
+  -- })
 end
 
 return M
