9e61f2626ea0a13fe9ef0902d2146d079e163785
Michael Bøcker-Larsen <mbl@code.boutique>
2025-02-18 14:06:22 +0800

    nvim: add Format command

--- .config/nvim/lua/custom/formatting.lua
+++ .config/nvim/lua/custom/formatting.lua
@@ -1,6 +1,14 @@
-local prettier = "prettier"
-local js_langs = "prettier"
--- local js_langs = "eslint_d"
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format { async = true, lsp_format = "fallback", range = range }
+end, { range = true })
 
 require("conform").setup {
   formatters = {
@@ -14,15 +22,14 @@ require("conform").setup {
     ["*"] = { "trim_newlines", "typos" },
     gleam = { "trim_whitespace", "gleam" },
     graphql = { "prettier" },
-    javascript = { "trim_whitespace", js_langs },
-    javascriptreact = { "trim_whitespace", js_langs },
+    javascript = { "trim_whitespace", "prettier" },
+    javascriptreact = { "trim_whitespace", "prettier" },
     json = { "trim_whitespace", "jq" },
     lua = { "trim_whitespace", "stylua" },
-    markdown = { prettier },
+    markdown = { "prettier" },
     python = { "ruff_format" },
-    -- python = { "trim_whitespace", "isort", "black" },
-    typescript = { "trim_whitespace", js_langs },
-    typescriptreact = { "trim_whitespace", js_langs },
+    typescript = { "trim_whitespace", "prettier" },
+    typescriptreact = { "trim_whitespace", "prettier" },
     yaml = { "trim_whitespace", "prettier" },
   },
   format_on_save = {
